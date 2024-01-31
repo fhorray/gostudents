ce5c42968af997f29af53e073167ecb02271e749
Francy Santos <francy.horray@gmail.com>
2024-01-31 11:54:12 -0300

    added new routes

--- main.go
+++ main.go
@@ -1,48 +1,18 @@
 package main
 
 import (
-	"net/http"
+	"studentsapi/router"
 
-	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
-// Student representa a estrutura de dados de um estudante
-type Student struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
-	Grade int    `json:"grade"`
-}
 
-var students []Student
 
 func main() {
-	students = append(students, Student{ID: "1", Name: "Francy", Age: 25, Grade: 8})
-	students = append(students, Student{ID: "2", Name: "marcos", Age: 27, Grade: 9})
+	router.Students = append(router.Students, router.Student{ID: uuid.New().String(), Name: "Francy", Age: 25, Grade: 8})
+	router.Students = append(router.Students, router.Student{ID: uuid.New().String(), Name: "marcos", Age: 27, Grade: 9})
 
-	router := gin.Default()
-
-	router.GET("/students", getStudents)
-	router.POST("/students/", createStudent)
-
-	router.Run(":8080")
+	router.Initialize()
 }
 
-func getStudents(c *gin.Context) {
-	c.JSON(http.StatusOK, students)
-}
-
-func createStudent(c *gin.Context) {
-	var newStudent Student
-	if err := c.BindJSON(&newStudent); err != nil {
-		c.JSON(http.StatusBadGateway, gin.H{"error": err.Error()})
-		return
-	}
-
-	newStudent.ID = uuid.New().String()
-
-	students = append(students, newStudent)
-	c.JSON(http.StatusCreated, newStudent)
-}
 
